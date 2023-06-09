8920ca0b054f1793c54e7639197a7103e3f78016
Mark Harfouche <mark.harfouche@gmail.com>
2023-06-09 19:11:59 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,7 +17,15 @@ NCFLAGS=`nc-config --cflags --libs`
 RPATH=`wx-config --prefix`/lib
 
 # build the executable
-cd src && $CXX -std=c++11 -fpermissive -Wl,-rpath,${RPATH} -o ${PREFIX}/bin/ncvis ncvis.cpp kdtree.cpp wxNcVisFrame.cpp wxNcVisOptionsDialog.cpp wxNcVisExportDialog.cpp wxImagePanel.cpp GridDataSampler.cpp ColorMap.cpp netcdf.cpp ncvalues.cpp Announce.cpp TimeObj.cpp ShpFile.cpp schrift.cpp lodepng.cpp ${WXFLAGS} ${NCFLAGS}
+pushd src
+$CXX ${CXXFLAGS} -o ncvis \
+    ncvis.cpp kdtree.cpp wxNcVisFrame.cpp wxNcVisOptionsDialog.cpp wxNcVisExportDialog.cpp \
+    wxImagePanel.cpp GridDataSampler.cpp ColorMap.cpp netcdf.cpp ncvalues.cpp Announce.cpp \
+    TimeObj.cpp ShpFile.cpp schrift.cpp lodepng.cpp \
+    ${WXFLAGS} ${NCFLAGS}
+
+mkdir -p ${PREFIX}/bin
+cp ncvis ${PREFIX}/bin/ncvis
 
 # copy the resoruces folder to $PREFIX/share
 cd .. && mkdir ${PREFIX}/share/ncvis
